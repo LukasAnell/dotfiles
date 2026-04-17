6a60eba18ccef25a27619ab68ae3b05dbeea2d9c
LukasAnell <lukasmanell@gmail.com>
2026-04-16 21:16:50 -0400

    Initial dotfiles

---
+++ .config/nvim/init.lua
@@ -0,0 +1,54 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- File tree
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  -- LSP
+  { "neovim/nvim-lspconfig" },
+  -- Autocompletion
+  { "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" } },
+  -- Treesitter (better syntax highlighting)
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  -- Fuzzy finder
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  -- Status line
+  { "nvim-lualine/lualine.nvim" },
+  -- Colorscheme
+  { "folke/tokyonight.nvim" },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+})
+
+vim.cmd("colorscheme tokyonight")
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+vim.lsp.config('pyright', {})
+vim.lsp.config('clangd', {})
+vim.lsp.config('jdtls', {})
+vim.lsp.enable({'pyright', 'clangd', 'jdtls'})
+
+local cmp = require("cmp")
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+})
+
